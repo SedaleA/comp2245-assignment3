bafbca77e611c0534e9f6fb1dbfbf80d2f3cb9c2
Sedale <sedalep12@gmail.com>
2025-10-18 14:13:00 -0400

    Added End game and draw game triggers, corrected win text

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   let currentplayer = players[0];
   const getStatus = document.getElementById("status");
   const reset = document.querySelector(".btn");
+  // Adds a flag to prevent playing after game ends
+  let End = false;
+
+  // Winning Conditions
 
   const winn_con = [
     [0, 1, 2],
@@ -16,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     [0, 4, 8],
     [2, 4, 6],
   ];
-
+  // Reset Button Functionality
   reset.addEventListener("click", () => {
     getStatus.classList.remove("you-won");
     squares.forEach((square) => {
@@ -26,8 +30,9 @@ document.addEventListener("DOMContentLoaded", () => {
     currentplayer = players[0];
     getStatus.textContent =
       "Move your mouse over a square and click to play an X or an O.";
+    if (End) return;
   });
-
+  // Turns divs into squares and adds mouse hover and click functionality
   squares.forEach((square) => {
     square.classList.add("square");
     square.addEventListener("mouseenter", () => square.classList.add("hover"));
@@ -35,21 +40,28 @@ document.addEventListener("DOMContentLoaded", () => {
       square.classList.remove("hover")
     );
     square.addEventListener("click", () => {
+      if (End) return;
       if (square.textContent !== "") return;
 
       square.textContent = currentplayer;
       square.classList.add(currentplayer);
-
+      // Check for Winner or Draw
       if (Winner()) {
-        getStatus.textContent = `Nice ${currentplayer} wins`;
+        getStatus.textContent = `Congratulations! ${currentplayer} is the Winner!`;
         getStatus.classList.add("you-won");
+        End = true;
+        return;
+      }
+      if ([...squares].every((s) => s.textContent !== "")) {
+        getStatus.textContent = "Draw!";
+        End = true;
         return;
       }
 
       currentplayer = currentplayer === "X" ? "O" : "X";
     });
   });
-
+  // Winner Checking Function
   function Winner() {
     for (let i = 0; i < winn_con.length; i++) {
       const [a, b, c] = winn_con[i];
