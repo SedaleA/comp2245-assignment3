6a2b5db1adbcec45a0ff22c920e2ef4ca99f2b2c
Sedale <sedalep12@gmail.com>
2025-10-18 11:12:34 -0400

    added X,O function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const board = document.getElementById("board");
   const squares = board.querySelectorAll("div");
+  const players = ["X", "O"];
+  let currentplayer = players[0];
 
   squares.forEach((square) => {
     square.classList.add("square");
+    square.addEventListener("click", () => {
+      if (square.textContent !== "") return;
+
+      square.textContent = currentplayer;
+      square.classList.add(currentplayer);
+      currentplayer = currentplayer === "X" ? "O" : "X";
+    });
   });
 });
